6788136a370fb95de4294bd1c1e10005832b007e
YannLuo <yannluo@foxmail.com>
2018-11-13 20:20:36 +0800

    20181113

--- fuzzing/utils/reader.py
+++ fuzzing/utils/reader.py
@@ -1,4 +1,23 @@
+import os
+
+
 def read_file(fpath):
     with open(fpath, 'r', encoding='utf-8') as rf:
         content = rf.read()
     return content
+
+
+def read_pyfiles(dir_path):
+    
+    def _read_pyfiles(path):
+        if os.path.isdir(path):
+            files = os.listdir(path)
+            for file in files:
+                new_path = os.path.join(path, file)
+                _read_pyfiles(new_path)
+        elif os.path.isfile(path) and path.endswith('.py'):
+            ret.add(path)
+    
+    ret = set()
+    _read_pyfiles(dir_path)
+    return ret
